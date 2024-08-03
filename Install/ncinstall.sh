c01491d1f94661aeff6a85c0356c1f80f3ec8346
antonfranck <anton.franck@okeano.de>
2024-08-03 18:37:05 +0200

    Add Download Links

--- Install/ncinstall.sh
+++ Install/ncinstall.sh
@@ -8,17 +8,17 @@ echo "3. Neueste Version"
 read -p "Geben Sie die Nummer der gewünschten Version ein: " version
 
 if [ "$version" == "1" ]; then
-    echo "Nextcloud 27 wird installiert..."
-    # Hier kannst du den Code für die Installation von Nextcloud 27 einfügen
-    version="27" # Ändere den Wert der Variable version auf "27"
-elif [ "$version" == "2" ]; then
     echo "Nextcloud 28 wird installiert..."
+    # Hier kannst du den Code für die Installation von Nextcloud 27 einfügen
+    version="https://download.nextcloud.com/server/releases/nextcloud-28.0.8.zip" # Ändere den Wert der Variable version auf "27"
+elif [ "$version" == "2" ]; then
+    echo "Nextcloud 29 wird installiert..."
     # Hier kannst du den Code für die Installation von Nextcloud 28 einfügen
-    version="28" # Ändere den Wert der Variable version auf "28"
+    version="https://download.nextcloud.com/server/releases/nextcloud-29.0.4.zip" # Ändere den Wert der Variable version auf "28"
 elif [ "$version" == "3" ]; then
     echo "Neueste Version wird installiert..."
     # Hier kannst du den Code für die Installation der neuesten Version einfügen
-    version="latest" # Ändere den Wert der Variable version auf "latest"
+    version="https://download.nextcloud.com/server/releases/latest.zip" # Ändere den Wert der Variable version auf "latest"
 else
     echo "Ungültige Eingabe. Bitte geben Sie entweder 1, 2 oder 3 ein."
 fi
